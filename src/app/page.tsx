45cf875f39f9d6659b6a16e895184c325d71a05b
Joycered1 <missjoycered@gmail.com>
2026-04-28 20:23:10 +0100

    Smooth Scroll Effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Navbar from '@/components/Navbar/Navbar';
 import Hero from '@/components/Hero/Hero';
 import WhyTimon from '@/components/WhyTimon/WhyTimon';
@@ -5,8 +7,27 @@ import HowToSignUp from '@/components/HowToSignUp/HowToSignUp';
 import Testimonials from '@/components/Testimonials/Testimonials';
 import FAQ from '@/components/FAQ/FAQ';
 import Footer from '@/components/Footer/Footer';
+import { useEffect } from 'react';
+import Lenis from "lenis";
 
 export default function Home() {
+  useEffect(() => {
+		const lenis = new Lenis({
+			duration: 1.2,
+			smoothWheel: true,
+		});
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+
+		return () => {
+			lenis.destroy();
+		};
+	}, []);
   return (
     <>
       <Navbar />
